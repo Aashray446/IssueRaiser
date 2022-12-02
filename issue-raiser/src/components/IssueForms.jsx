d0a58ce55abf2764aad7410a76de3a64b2d31f76
Indira Kumar <indirakumarak@gmail.com>
2022-12-02 20:22:34 +0530

    Issue Request Page Completed

--- issue-raiser/src/components/IssueForms.jsx
+++ issue-raiser/src/components/IssueForms.jsx
@@ -89,9 +89,23 @@ const issueForms = () => {
             </label>
           </div>
         </div>
+        <div>
+          <label
+            for="message"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Your Complaint
+          </label>
+          <textarea
+            id="message"
+            rows="4"
+            class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Write your thoughts here..."
+          ></textarea>
+        </div>
         <button
           type="submit"
-          class="text-white bg-mid2-blue hover:bg-mid1-blue focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          class="text-white bg-mid2-blue hover:bg-mid1-blue focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center mt-2"
         >
           Submit
         </button>
