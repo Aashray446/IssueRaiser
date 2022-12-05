5dda725c41d8c24213577090b8387d3589cc22e1
Indira Kumar <indirakumarak@gmail.com>
2022-12-05 19:16:03 +0530

    Update Request Page Completed
    - All basic details and functions have been completed
    - Future possible updates: 1) post request url, 2) Get request and updating those values in the hook

---
+++ issue-raiser/src/components/UpdateRequest.jsx
@@ -0,0 +1,156 @@
+import React, { useState } from "react";
+
+const UpdateRequest = () => {
+  // Here instead of this object,
+  // we will receive the request id, and make a call to backend
+  //to fetch the patient and complaint details, and those details will be replaced in the empty " " values initially.
+  const URL = "";
+  const [patientData, setPatientData] = useState({
+    firstName: "",
+    lastName: "",
+    phoneNumber: "",
+    inPatientNumber: "",
+    email: "",
+    priority: "",
+    complaint: "",
+  });
+  function handleDataChange(e) {
+    const newData = { ...patientData };
+    newData[e.target.id] = e.target.value;
+    setPatientData(newData);
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    axios.post(URL, patientData);
+  }
+  return (
+    <div className="grid h-screen place-items-center">
+      <form
+        className="mt-5 w-96 bg-regal-blue p-8 rounded-lg"
+        action="post"
+        onSubmit={(e) => handleSubmit(e)}
+      >
+        <div className="relative z-0 mb-6 w-full group">
+          <input
+            type="email"
+            name="email"
+            id="email"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+            value={patientData.email}
+            onChange={(e) => handleDataChange(e)}
+            required
+          />
+          <label
+            for="email"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Email address
+          </label>
+        </div>
+        <div className="grid md:grid-cols-2 md:gap-6">
+          <div className="relative z-0 mb-6 w-full group">
+            <input
+              type="text"
+              name="firstName"
+              id="firstName"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              value={patientData.firstName}
+              onChange={(e) => handleDataChange(e)}
+              required
+            />
+            <label
+              for="firstName"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              First name
+            </label>
+          </div>
+          <div className="relative z-0 mb-6 w-full group">
+            <input
+              type="text"
+              name="lastName"
+              id="lastName"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              value={patientData.lastName}
+              onChange={(e) => handleDataChange(e)}
+              required
+            />
+            <label
+              for="lastName"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Last name
+            </label>
+          </div>
+        </div>
+        <div className="grid md:grid-cols-2 md:gap-6">
+          <div className="relative z-0 mb-6 w-full group">
+            <input
+              type="tel"
+              pattern="[0-9]{3}[0-9]{3}[0-9]{4}"
+              name="phoneNumber"
+              id="phoneNumber"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              value={patientData.phoneNumber}
+              onChange={(e) => handleDataChange(e)}
+              required
+            />
+            <label
+              for="phoneNumber"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Phone number
+            </label>
+          </div>
+          <div className="relative z-0 mb-6 w-full group">
+            <input
+              type="text"
+              name="inPatientNumber"
+              id="inPatientNumber"
+              pattern="[0-9]{5}"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              value={patientData.inPatientNumber}
+              onChange={(e) => handleDataChange(e)}
+              required
+            />
+            <label
+              for="inPatientNumber"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              In Patient Number
+            </label>
+          </div>
+        </div>
+        <div>
+          <label
+            for="message"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Your Complaint
+          </label>
+          <textarea
+            id="complaint"
+            rows="4"
+            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Write your complaint here..."
+            value={patientData.complaint}
+            onChange={(e) => handleDataChange(e)}
+          ></textarea>
+        </div>
+        <button
+          type="submit"
+          className="text-white bg-mid2-blue hover:bg-mid1-blue focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center mt-2"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default UpdateRequest;
