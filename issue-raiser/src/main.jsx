2c3f2e2260471d6a8e098e6a1fba6258ef7e8563
Indira Kumar <indirakumarak@gmail.com>
2022-12-05 19:20:38 +0530

    Route for IssueForms, view issues, update issues added
    - Routes for four new pages(IssueForms, IssueLandingPage, ViewRequest, UpdateRequest) added.

--- issue-raiser/src/main.jsx
+++ issue-raiser/src/main.jsx
@@ -1,19 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import ErrorPage from "./pages/errorPage";
 import Home from "./pages/home";
 import "./index.css";
-
+import IssueForms from "./components/IssueForms";
+import IssueLandingPage from "./components/IssueLandingPage";
+import ViewRequest from "./components/ViewRequest";
+import UpdateRequest from "./components/UpdateRequest";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
-    errorElement: <ErrorPage/>,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/issue",
+    element: <IssueForms />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/landingPage",
+    element: <IssueLandingPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/viewRequest",
+    element: <ViewRequest />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/updateRequest",
+    element: <UpdateRequest />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
